f2e9c0f3d78c420efe1ccf2e1519a5de86345965
ColinMinielly <miniellyc@gmail.com>
2024-12-17 16:36:06 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ async function fetchData() {
     const response = await fetch('data.json');
     const data = await response.json();
 
+    // Weekly Activity
+    document.getElementById('weeklyCalories').innerText = data.weeklyActivity.totalCalories;
+    document.getElementById('topActivity').innerText = data.weeklyActivity.topActivity;
+
+    // Hydration and Nutrition
     document.getElementById('waterConsumed').innerText = data.hydration.waterConsumed;
     document.getElementById('proteinIntake').innerText = data.hydration.proteinIntake;
+
+    // Stress and Mood
     document.getElementById('stressLevel').innerText = 'Stress Level: ' + data.stressMood.stressLevel;
     document.getElementById('mood').innerText = 'Mood: ' + data.stressMood.mood;
+
+    // Reflection Journal
     document.getElementById('reflectionDate').innerText = data.reflectionJournal.lastEntry;
+
+    // Weight Milestones
+    document.getElementById('weightMilestone').innerText = data.weightProgress.currentProgress;
 }
 window.onload = fetchData;
