514d2b0c1855179ff6fc8619d901ec9d3fc13fc5
ColinMinielly <miniellyc@gmail.com>
2024-12-17 16:14:38 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -3,27 +3,12 @@
         const response = await fetch('data.json');
         const data = await response.json();
 
-        // Event Countdown
         document.getElementById('eventCountdown').innerText = data.eventCountdown + ' Days to Go';
-
-        // Weight Progress
-        data.weightProgress.forEach((goal, index) => {
-            document.getElementById('weightGoal' + index).innerText = goal.goal + ': ' + goal.progress + '% Complete';
-            document.getElementById('progressBar' + index).style.width = goal.progress + '%';
-        });
-
-        // Hydration and Nutrition
         document.getElementById('waterConsumed').innerText = data.hydration.waterConsumed;
         document.getElementById('proteinIntake').innerText = data.hydration.proteinIntake;
-
-        // Stress and Mood
-        document.getElementById('stressLevel').innerText = 'Stress Level: ' + data.stressMood.stressLevel;
+        document.getElementById('stressLevel').innerText = 'Stress: ' + data.stressMood.stressLevel;
         document.getElementById('mood').innerText = 'Mood: ' + data.stressMood.mood;
-
-        // Weekly Activity
-        document.getElementById('caloriesBurned').innerText = 'Total Calories Burned: ' + data.latestActivity.caloriesBurned;
-        document.getElementById('mostActive').innerText = 'Most Active Task: ' + data.latestActivity.mostActive;
+        document.getElementById('reflectionDate').innerText = data.reflectionJournal.lastEntry;
     }
-
     window.onload = fetchData;
     
